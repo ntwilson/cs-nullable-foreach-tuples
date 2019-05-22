25a40da4a89d03d9de9dd9547f94f132757119c8
nathan <nathan.wilson@marquetteenergyanalytics.com>
2019-05-22 09:20:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cs_nullable_foreach_tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xs = new[] { 
+                (a: 1, b: 2),
+                (a: 3, b: 4)
+            };
+
+            foreach (var (a, b) in xs) { }
+            foreach (var tuple in xs) {
+                var (a, b) = tuple;
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
